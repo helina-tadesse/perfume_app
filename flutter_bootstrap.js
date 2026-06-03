e4c1d87dc9efb93f920fb338232c46847553be52
Helina <helinatade27@gmail.com>
2026-06-03 14:19:13 +0300

    feat: add favorites and scent layering pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1402917459" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1814298810" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
